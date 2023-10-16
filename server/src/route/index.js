193c483116d1d55d600b9948f9f109c1c7685be4
mohan-potter <mohanrajvengi@gmail.com>
2023-10-16 18:21:53 +0530

    🐛  fix : cors error

--- server/src/route/index.js
+++ server/src/route/index.js
@@ -1,10 +1,23 @@
-import { Hono } from "hono";
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
-import { gitHookRoute } from "./githook.js";
-import { dashboardRoute } from "./dashboard.js";
+import { gitHookRoute } from './githook.js';
+import { dashboardRoute } from './dashboard.js';
 
 const app = new Hono();
-app.route("/pr-stats/api/githook", gitHookRoute);
-app.route("/pr-stats/api/",dashboardRoute);
+
+app.use('/pr-stats/api/*', cors());
+// app.use(
+// 	'/pr-stats/api/*',
+// 	cors({
+// 		origin: 'http://localhost:3000',
+// 		allowMethods: ['POST', 'GET', 'OPTIONS'],
+// 		exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+// 		maxAge: 600,
+// 		credentials: true,
+// 	})
+// );
+app.route('/pr-stats/api/githook', gitHookRoute);
+app.route('/pr-stats/api/', dashboardRoute);
 
 export { app };
