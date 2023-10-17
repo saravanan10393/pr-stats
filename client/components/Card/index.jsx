488ca49be8c9f984936a76e4a9fbfcb90ca29367
Venkadesh S <venkadesh@kissflow.com>
2023-10-17 08:14:40 +0530

    review and alignment changes

--- client/components/Card/index.jsx
+++ client/components/Card/index.jsx
@@ -3,25 +3,25 @@ import Image from "next/image";
 import { Card } from "@tremor/react";
 
 export default function CardComponent({ reviewerInfo, onClick }) {
-  const { name, avatarUrl, totalReviewedPr, avgReviewTime } = reviewerInfo;
+	const { name, avatarUrl, totalReviewedPr, avgReviewTime } = reviewerInfo;
 
-  return (
-    <Card onClick={onClick} className="animate-swipeIn flex gap-4 mb-4 p-2">
-      <Image
-        src="/avatar.jpg"
-        width={70}
-        height={70}
-        alt="Picture of the author"
-      />
-      <div className="grid grid-cols-3 w-full">
-        <div className="col-span-3 font-semibold text-l">{name}</div>
-        <div className="col-span-1 font-normal text-base">
-          Total reviewed pr: {totalReviewedPr}
-        </div>
-        <div className="col-span-1 font-normal text-base">
-          Average reviewed time : {avgReviewTime}
-        </div>
-      </div>
-    </Card>
-  );
+	return (
+		<Card onClick={onClick} className="animate-swipeIn flex gap-4 mb-4 p-2">
+			<Image
+				src="/avatar.jpg"
+				width={70}
+				height={70}
+				alt="Picture of the author"
+			/>
+			<div className="grid grid-cols-3 w-full">
+				<div className="col-span-3 font-semibold text-l">{name}</div>
+				<div className="col-span-1 font-normal text-base">
+					Total reviewed pr: <span>{totalReviewedPr}</span>
+				</div>
+				<div className="col-span-1 font-normal text-base">
+					Average reviewed time : <span>{avgReviewTime}</span>
+				</div>
+			</div>
+		</Card>
+	);
 }
--- client/pages/dashboard/reviewerslist.jsx
+++ client/pages/dashboard/reviewerslist.jsx
@@ -7,40 +7,40 @@ import { MOCK_DATA } from "../mockdata";
 import { Header, Toolbar, CardComponent } from "../../components";
 
 export default function ReviewersList() {
-  const router = useRouter();
+	const router = useRouter();
 
-  const [reviewersList, setReviewersList] = useState(MOCK_DATA);
-  const [searchTerm, setSearchTerm] = useState("");
+	const [reviewersList, setReviewersList] = useState(MOCK_DATA);
+	const [searchTerm, setSearchTerm] = useState("");
 
-  const handleSearch = (event) => {
-    const searchValue = event.target.value;
-    setSearchTerm(searchValue);
-    if (searchValue === "") {
-      setReviewersList(MOCK_DATA);
-    }
-    setReviewersList((list) => {
-      return list.filter((data) => data.name.includes(searchValue));
-    });
-    console.log(event.target.value);
-  };
+	const handleSearch = (event) => {
+		const searchValue = event.target.value;
+		setSearchTerm(searchValue);
+		if (searchValue === "") {
+			setReviewersList(MOCK_DATA);
+		}
+		setReviewersList((list) => {
+			return list.filter((data) => data.name.includes(searchValue));
+		});
+		console.log(event.target.value);
+	};
 
-  return (
-    <div className="h-screen flex flex-col">
-      <Header />
-      <Toolbar
-        title={"User list"}
-        searchTerm={searchTerm}
-        handleSearch={handleSearch}
-      />
-      <div className="h-full overflow-y-auto	grid auto-rows-max p-4">
-        {reviewersList.map((reviewerInfo) => (
-          <CardComponent
-            onClick={() => router.push(`./reviewerinfo/${reviewerInfo.id}`)}
-            reviewerInfo={reviewerInfo}
-            key={reviewerInfo.id}
-          />
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="h-screen flex flex-col">
+			<Header pageTitle={"Reviewers list"} />
+			<Toolbar
+				title={"User list"}
+				searchTerm={searchTerm}
+				handleSearch={handleSearch}
+			/>
+			<div className="h-full overflow-y-auto	grid auto-rows-max p-4">
+				{reviewersList.map((reviewerInfo) => (
+					<CardComponent
+						onClick={() => router.push(`./reviewerinfo/${reviewerInfo.id}`)}
+						reviewerInfo={reviewerInfo}
+						key={reviewerInfo.id}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
